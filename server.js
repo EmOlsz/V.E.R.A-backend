d14eb7ae6363f079fdf5b135a59b3af97ce9f35c
Maksymilian Olszewski <olszewski.maksymilian@gmail.com>
2021-01-25 23:34:58 +0000

    added new routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import moment from 'moment';
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/vera";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -13,15 +14,59 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const Feedback = new mongoose.model('Feedback', {
-  name: String,
-  description: String
+// MODELS
+
+const Feeding = new mongoose.model('Feeding', {
+  breast: String,
+  timer: Number,
+  date: {
+    type: Date,
+    default: () => new Date()
+  }
 });
 
-app.post('/feedback', async (req, res) => {
-  const feedback = new Feedback(req.body);
-  await feedback.save();
-  res.json(feedback);
+const Quote = new mongoose.model('Quote', {
+  value: String
+});
+
+// FEEDING
+
+app.post('/feeding', async (req,res) => {
+  const newFeeding = new Feeding(req.body);
+  await newFeeding.save();
+  res.json(newFeeding);
+});
+
+app.get('/feeding/daily/:breast', async (req, res) => {
+  const { breast } = req.params;
+  const today = moment().startOf('day');
+
+  const feedingList = await Feeding.find({
+    breast,
+    date: {
+      $gte: today.toDate(),
+      $lte: moment(today).endOf('day').toDate()
+    }
+  });
+
+  res.json(feedingList);
+});
+
+// QUOTES
+
+app.post('/quotes', async (req, res) => {
+  const newQuote = new Quote(req.body);
+  await newQuote.save();
+
+  res.json(newQuote);
+});
+
+
+app.get('/quotes', async (req, res) => {
+  const quoteList = await Quote.find();
+  const randomQuote = quoteList[Math.floor(Math.random() * (quoteList.length + 1))];
+
+  res.json(randomQuote);
 });
 
 app.listen(port, () => {
